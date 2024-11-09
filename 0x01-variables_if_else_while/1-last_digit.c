8c497b1eb90be8d2f4eecf91bf2ed27cc91bbe00
kenechiii <nwabugofavour@gmail.com>
2024-11-09 08:15:11 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ * /
+
+int main(void)
+
+int main(void)
+{
+	int n;
+	int lastnum;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastnum = n % 10;
+	if (lastnum > 5)
+	{
+printf(:
+	/* your code goes there */
+	return (0);
+}
